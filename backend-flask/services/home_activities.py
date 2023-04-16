c92cdf0cd4b70f9c92537d41ec30d3c18989256e
Loretta <101978292+Rietta1@users.noreply.github.com>
2023-04-16 13:53:46 +0000

    refactor the db library

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 import logging
 
-from lib.db import pool, query_wrap_object, query_wrap_array
+from lib.db import db
 
 tracer = trace.get_tracer("home.activites")
 
@@ -11,31 +11,17 @@ class HomeActivities:
     print("HOME ACTIVITY")
     #logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activites-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
-
-      sql = query_wrap_array("""
+      # span = trace.get_current_span()
+      # now = datetime.now(timezone.utc).astimezone()
+      # span.set_attribute("app.now", now.isoformat())
+      results = db.query_array_json("""
+      
       SELECT
         activities.uuid,
         users.display_name,
-        users.handle,
-        activities.message,
-        activities.replies_count,
-        activities.reposts_count,
-        activities.likes_count,
-        activities.reply_to_activity_uuid,
-        activities.expires_at,
-        activities.created_at
+        
       FROM public.activities
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
       """)
-      print(sql)
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      return json[0]
+      return results
